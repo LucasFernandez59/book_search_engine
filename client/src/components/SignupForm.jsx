031b60d026e5968a0dd3098ea7aff5fcddf6773d
LucasF <che.fernandez255@gmail.com>
2023-12-02 18:29:08 -0700

    updated code

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, {error}]= {ADD_USER};
+  const [addUser, {error}]= useMutation(ADD_USER);
   
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -21,6 +21,7 @@ const SignupForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(userFormData)
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
@@ -30,7 +31,7 @@ const SignupForm = () => {
     }
 
     try {
-      const {data} = await addUser({
+      const { data } = await addUser({
         variables: {...userFormData}
       });
 
